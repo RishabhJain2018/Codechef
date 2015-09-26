f2eff8052a054c13575ee192f98ab75cb510f636
RishabhJain2018 <jain.rishabh170895@gmail.com>
2015-09-26 14:43:19 +0530

    Added: Beginner Problems

---
+++ FCTRL2.py
@@ -0,0 +1,8 @@
+a=int(raw_input())
+for i in range(1,a+1):
+	b=int(raw_input())
+	l=1;
+	for j in range(1,b+1):
+		l=l*j;
+	print l
+
---
+++ TSORT.py
@@ -0,0 +1,9 @@
+a=int(raw_input())
+list=[]
+for i in range(0,a):
+	list+=[int(raw_input())]
+list.sort()
+for i in list:
+	print i
+
+	
